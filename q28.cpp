67794576a959a1ae5443a2111bb9ee605bb62e47
st010447 <st010447@gmail.com>
2021-10-21 11:02:56 +0800

    fuck u

---
+++ q28.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        if (needle == "")
+        {
+            return 0;
+        }
+        int j = 0;
+
+        int keep = 0;
+        int correct = 0;
+        int first_hit = 0;
+        int n = haystack.size();
+        for (int i = 0; i < n; i++)
+        {
+
+            if (haystack[i] == needle[j])
+            {
+                if (first_hit == 0)
+                {
+                    first_hit = 1;
+                    keep = i;
+                }
+                correct++;
+                j++;
+            }
+            else
+            {
+                if (first_hit)
+                {
+                    i = keep;
+                    // cout << haystack[i];
+                    first_hit = 0;
+                }
+                correct = 0;
+                j = 0;
+                keep = 0;
+            }
+            if (correct == needle.length())
+            {
+
+                return i - needle.length() + 1;
+            }
+            // cout << i << " " << first_hit << endl;
+        }
+        return -1;
+    }
+};
+main()
+{
+    Solution A;
+    cout << endl
+         << A.strStr("aabaaabaaac",
+                     "aabaaac");
+}
