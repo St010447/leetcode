2b86c4b8092f21898e478ab804c9cf5469a1a50d
pekoFan <peko>
2021-10-28 11:44:38 +0800

    q58 finish

--- q58.cpp
+++ q58.cpp
@@ -1,13 +1,32 @@
-#include <string>
-#include <map>
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int space_pos = 0;
-    map<int, string> temp;
-    int lengthOfLastWord(string s)
-    {
+    string temp;
+    vector <string> ans;
+    int lengthOfLastWord(string s) {
+        s.insert(s.begin(),' ');
+        s.insert(s.end(),' ');
+        for(int i = 0; i < s.size(); i++){
+            if(s[i]!=' '){
+                temp+=s[i];
+            }
+            else{
+                ans.push_back(temp);
+                temp="";
+            }
+        }
+    // cout << ans[ans.size()-1];
+    
+        return (ans[ans.size()-1]).length();
     }
 };
+
+int main(){
+    Solution A;
+    cout<<A.lengthOfLastWord("test 1 2 3 445454");
+    return 0;
+}
